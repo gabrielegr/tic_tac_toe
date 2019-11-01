1f61c9f6854b06ea7443f8adddf0c9f784bb6490
gabrielegr <00199518@uca.edu.sv>
2019-11-01 11:35:15 -0600

    Updates

---
+++ precache-manifest.f5ce5791fca80be478bbc339ad8fb828.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fda85abbb8d82226f0297415a95b2ea",
+    "url": "/tic_tac_toe/index.html"
+  },
+  {
+    "revision": "6023d3fe19fb433d3e43",
+    "url": "/tic_tac_toe/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "0ed91b759889665edf14",
+    "url": "/tic_tac_toe/static/js/2.94380357.chunk.js"
+  },
+  {
+    "revision": "6023d3fe19fb433d3e43",
+    "url": "/tic_tac_toe/static/js/main.603fc3a4.chunk.js"
+  },
+  {
+    "revision": "89166832caf18d6d212c",
+    "url": "/tic_tac_toe/static/js/runtime-main.6ef6ad6d.js"
+  }
+]);
